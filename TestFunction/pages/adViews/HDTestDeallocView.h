c9ccf9bff1f12809ed39a8e645c688b27086b5ee
huaida <1554417228@qq.com>
2019-07-30 16:31:14 +0800

    调整代码

---
+++ TestFunction/pages/adViews/HDTestDeallocView.h
@@ -0,0 +1,17 @@
+//
+//  HDTestDeallocView.h
+//  TestFunction
+//
+//  Created by 怀达 on 2019/7/29.
+//  Copyright © 2019 white. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HDTestDeallocView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestFunction/pages/adViews/HDTestDeallocViewController.m
@@ -0,0 +1,32 @@
+//
+//  HDTestDeallocViewController.m
+//  TestFunction
+//
+//  Created by 怀达 on 2019/7/29.
+//  Copyright © 2019 white. All rights reserved.
+//
+
+#import "HDTestDeallocViewController.h"
+
+@interface HDTestDeallocViewController ()
+
+@end
+
+@implementation HDTestDeallocViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
