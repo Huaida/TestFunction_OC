fead43e612f7d215deb0ba68b9f7f6a9a98e4c62
huaida <1554417228@qq.com>
2019-08-08 17:46:30 +0800

    测试对系统navigationBar背景图的设置

--- TestFunction/pages/navigationBar/view/HDNavBarThreeViewController.m
+++ TestFunction/pages/navigationBar/view/HDNavBarThreeViewController.m
@@ -17,23 +17,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
     [self setBaseProperty];
+    [self setNavigationBarImage];
 }
 - (void)setBaseProperty{
-    self.view.backgroundColor = [UIColor grayColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    [self wr_setNavBarBackgroundAlpha:0];
-    [self wr_setNavBarShadowImageHidden:YES];
-}
-/*
-#pragma mark - Navigation
+//    [self wr_setNavBarBackgroundAlpha:0];
+//    [self wr_setNavBarShadowImageHidden:YES];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
+- (void)setNavigationBarImage{
+//        self.navigationController.navigationBar.shadowImage = [UIImage imageNamed:@"testImageView"];
+//    UINavigationBar *bar = self.navigationController.navigationBar;
+//    bar.translucent = NO;
+//    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"testImageView"] forBarMetrics:UIBarMetricsCompactPrompt];
+    
+    self.navigationItem.prompt = @"Navigation prompts appear at the top.";
+    UIImage *image1 = [UIImage imageNamed:@"testImageView"];
+    UIImage *image2 = [UIImage imageNamed:@"bgImage"];
+    UIImageView *iView1 = [[UIImageView alloc] initWithImage:image1];
+    UIImageView *iView2 = [[UIImageView alloc] initWithImage:image2];
+    iView1.frame = CGRectMake(0, 100, 50, 50);
+    iView2.frame = CGRectMake(100, 100, 50, 50);
+    [self.view addSubview:iView1];
+    [self.view addSubview:iView2];
+    UINavigationBar *navBar = self.navigationController.navigationBar;
+    [navBar setBackgroundImage:image1 forBarMetrics:UIBarMetricsDefault];
+    [navBar setBackgroundImage:image2 forBarMetrics:UIBarMetricsCompact];
+}
+- (UIImage*)imageWithColor:(UIColor*)color
+
+{
+    
+    CGRect rect = CGRectMake(0.0f, 0.0f, 8.0f, 8.0f);
+    
+    UIGraphicsBeginImageContext(rect.size);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    
+    CGContextFillRect(context, rect);
+    
+    UIImage*theImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return theImage;
+    
+}
+
 
 @end
