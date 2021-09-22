79e9e660e624e023b0caf52453dfa29d7ade2f11
Cage <1554417228@qq.com>
2021-09-22 18:14:22 +0800

    调整TableView的内容

--- TestFunction/pages/自定义转场/AfteriOS8/HDNewCustomTransitionVC.m
+++ TestFunction/pages/自定义转场/AfteriOS8/HDNewCustomTransitionVC.m
@@ -8,7 +8,7 @@
 
 #import "HDNewCustomTransitionVC.h"
 
-@interface HDNewCustomTransitionVC ()
+@interface HDNewCustomTransitionVC ()<UITableViewDataSource,UITableViewDelegate>
 
 @end
 
@@ -17,11 +17,149 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor greenColor];
-    self.navigationController.navigationBar.hidden = NO;
-    self.navigationController.navigationBar.opaque = YES;
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.backgroundColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    
+    [self customAddSubviews];
+}
+-(void)customAddSubviews{
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    [self.view addSubview:tableView];
+    tableView.dataSource = self;
+    tableView.delegate = self;
+//    tableView.allowsMultipleSelection = YES;
+    tableView.editing = YES;
+    
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:NSStringFromClass([UITableViewCell class])];
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell =[ tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class])];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:NSStringFromClass([UITableViewCell class])];
+    }
+    switch (indexPath.section) {
+        case 0:
+        {
+            switch (indexPath.row) {
+                case 0:
+                    cell.textLabel.text = @"Cross Dissolve";
+                    break;
+                case 1:
+                    cell.textLabel.text = @"Swipe";
+                    break;
+                case 2:
+                    cell.textLabel.text = @"Custom Presentation";
+                    break;
+                case 3:
+                    cell.textLabel.text = @"Adaptive Presentation";
+                    break;
+                default:
+                    break;
+            }
+        }
+        case 1:
+        {
+            switch (indexPath.row) {
+                case 0:
+                    cell.textLabel.text = @"Checkerboard";
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+            break;
+        case 2:
+        {
+            switch (indexPath.row) {
+                case 0:
+                    cell.textLabel.text = @"Slide";
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+            break;
+            
+        default:
+            break;
+    }
+    
+    return cell;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSInteger number = 0;
+    switch (section) {
+        case 0:
+            number = 4;
+            break;
+        case 1:
+            number = 1;
+            break;
+        case 2:
+            number = 1;
+            break;
+            
+        default:
+            break;
+    }
+    return number;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;{
+    return 3;
+}
+//// Default is 1 if not implemented
+//
+- (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section; {
+    NSString *header = nil;
+    switch (section) {
+        case 0:
+            header = @"Presentation Transitions";
+            break;
+        case 1:
+            header = @"Navigation Controller Transitions";
+            break;
+        case 2:
+            header = @"TabBar Controller Transitions";
+            break;
+            
+        default:
+            break;
+    }
+    return header;
+}   // fixed font style. use custom view (UILabel) if you want something different
+- (nullable NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section;{
+    return @"";
+}
+//
+//// Editing
+//
+//// Individual rows can opt out of having the -editing property set for them. If not implemented, all rows are assumed to be editable.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath;{
+    return YES;
+}
+//
+//// Moving/reordering
+//
+//// Allows the reorder accessory view to optionally be shown for a particular row. By default, the reorder control will be shown only if the datasource implements -tableView:moveRowAtIndexPath:toIndexPath:
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath;{
+    return YES;
+}
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    
+}
+- (NSIndexPath *)tableView:(UITableView *)tableView targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath{
+    return proposedDestinationIndexPath;
+}
+//必须把编辑模式改成None，默认的是delete
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
 
-
+{
+    
+    return UITableViewCellEditingStyleNone;
+    
+}
 @end
