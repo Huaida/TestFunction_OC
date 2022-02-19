80ab6f0c7cc3c065ad2346a940de9f3cf54241bf
DanielHuai <1554417228@qq.com>
2022-02-19 18:20:09 +0800

    dev 123

--- TestFunction/pages/Home/View/HDHomeViewController.m
+++ TestFunction/pages/Home/View/HDHomeViewController.m
@@ -36,6 +36,11 @@
 ///4
 ///5
 
+///
+///1
+///2
+///3
+
 #define MakeColorRGB(hex)  ([UIColor colorWithRed:((hex>>16)&0xff)/255.0 green:((hex>>8)&0xff)/255.0 blue:(hex&0xff)/255.0 alpha:1.0])
 
 typedef void (^someBlock)(void);
